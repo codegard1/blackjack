60e54995ecbd80a64201c872bc241c1155882689
Chris <c.odegard@gmail.com>
2017-07-03 12:00:19 -0400

    refactored StatusDisplay

--- src/StatusDisplay.js
+++ src/StatusDisplay.js
@@ -2,38 +2,22 @@ import React from "react";
 import * as T from "prop-types";
 
 export const StatusDisplay = props => {
-  const gameStatusDisplay = (
-    <span className="ms-font-s" style={{ float: "left" }}>
-      Game Status: <strong>{props.gameStatus}</strong>
-      <br />
-      Turn Count: {props.turnCount}
-    </span>
-  );
-
-  const currentPlayerDisplay = (
-    <p className="ms-font-s">
-      <span>{`Player: ${props.player.title}`}</span> <br />
-      <span>{`Status: ${props.player.status}`}</span><br />
-      <span
-      >{`Hand Value: ${props.player.handValue.aceAsTen} / ${props.player.handValue.aceAsOne}`}</span>
-      <br />
-      <span>{`Turn: ${props.player.turn}`}</span><br />
-    </p>
-  );
-
-  return (
-    <div id="StatusPanel">
-      {gameStatusDisplay}
-      <br /><br />
-      {currentPlayerDisplay}
-    </div>
-  );
+    return (
+        <div id="StatusPanel" className="ms-font-s">
+            <span>Player: {props.player.title}</span><br />
+            <span>Status: {props.player.status}</span><br />
+            <span>Hand Value: {`${props.player.handValue.aceAsTen} / ${props.player.handValue.aceAsOne}`}</span><br />
+            <span>Turn: {props.player.turn || 'false'}</span><br />
+            <span>Game Status: {props.gameStatus || 0}</span><br />
+            <span>Turn Count: {props.turnCount || 0}</span><br />
+        </div>
+    );
 };
 
 StatusDisplay.propTypes = {
-  gameStatus: T.number,
-  turnCount: T.number,
-  player: T.object
+    gameStatus: T.number,
+    turnCount: T.number,
+    player: T.object
 };
 
 export default StatusDisplay;
