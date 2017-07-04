b3d51e53bf1a4252ab2ff918bf2ed8d631c81d0e
Chris Odegard <c.odegard@gmail.com>
2017-07-03 20:57:28 -0400

    decided not to do #3

--- src/DeckContainer.js
+++ src/DeckContainer.js
@@ -2,117 +2,136 @@ import React, { Component } from "react";
 import * as T from "prop-types";
 import Masonry from "react-masonry-component";
 import CardContainer from "./CardContainer";
-import StatusDisplay from './StatusDisplay'
-import { Callout } from 'office-ui-fabric-react/lib/Callout';
+import StatusDisplay from "./StatusDisplay";
+import { Callout } from "office-ui-fabric-react/lib/Callout";
 
 export class DeckContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { deckIsVisible: true, isCalloutVisible: false };
+  constructor(props) {
+    super(props);
+    this.state = {
+      deckIsVisible: true,
+      isStatusCalloutVisible: false
+    };
 
-        this._toggleDeck = this._toggleDeck.bind(this);
-        this._toggleCallout = this._toggleCallout.bind(this);
+    this._toggleDeck = this._toggleDeck.bind(this);
+    this._toggleStatusCallout = this._toggleStatusCallout.bind(this);
+  }
+
+  componentWillMount() {
+    if (this.props.hidden === true) {
+      this.setState({ deckIsVisible: false });
     }
+  }
 
-    componentWillMount() {
-        if (this.props.hidden === true) {
-            this.setState({ deckIsVisible: false });
-        }
-    }
+  _toggleStatusCallout() {
+    this.setState({
+      isStatusCalloutVisible: !this.state.isStatusCalloutVisible
+    });
+  }
 
-    _toggleCallout() {
-        this.setState({ isCalloutVisible: !this.state.isCalloutVisible });
-    }
+  _toggleDeck() {
+    this.setState({ deckIsVisible: !this.state.deckIsVisible });
+  }
 
-    _toggleDeck() {
-        this.setState({ deckIsVisible: !this.state.deckIsVisible });
-    }
+  render() {
+    // Options passed into the Masonry component
+    const masonryOptions = {
+      transitionDuration: "0.3s",
+      itemSelector: ".card",
+      columnWidth: ".card",
+      fitWidth: true
+    };
 
-    render() {
-        // Options passed into the Masonry component
-        const masonryOptions = {
-            transitionDuration: "0.3s",
-            itemSelector: ".card",
-            columnWidth: ".card",
-            fitWidth: true
-        };
+    // Create CardContainers to display cards
+    const childElements = this.props.deck
+      ? this.props.deck.map(card => {
+          return (
+            <CardContainer
+              key={card.suit + "-" + card.description}
+              {...card}
+              select={this.props.select}
+              deselect={this.props.deselect}
+              isSelectable={this.props.isSelectable}
+            />
+          );
+        })
+      : undefined;
 
-        // Create CardContainers to display cards
-        const childElements = this.props.deck
-            ? this.props.deck.map((card, index) => {
-                return (
-                    <CardContainer
-                        key={card.suit + "-" + card.description}
-                        {...card}
-                        select={this.props.select}
-                        deselect={this.props.deselect}
-                        isSelectable={this.props.isSelectable}
-                    />
-                );
-            })
-            : undefined;
+    // Set toggle icon for Deck titles
+    const toggleIcon = this.state.deckIsVisible
+      ? <i
+          className="ms-Icon ms-Icon--ChevronDown"
+          aria-hidden="true"
+          onClick={this._toggleDeck}
+        />
+      : <i
+          className="ms-Icon ms-Icon--ChevronUp"
+          aria-hidden="true"
+          onClick={this._toggleDeck}
+        />;
 
-        // Set toggle icon for Deck titles
-        const toggleIcon = this.state.deckIsVisible
-            ? <i className="ms-Icon ms-Icon--ChevronDown" aria-hidden="true" onClick={this._toggleDeck} />
-            : <i className="ms-Icon ms-Icon--ChevronUp" aria-hidden="true" onClick={this._toggleDeck} />;
+    // Set hand value text
+    const handValueDisplay =
+      this.props.handValue &&
+      <span className="handValue ms-font-xl">
+        Hand Value: {this.props.handValue.aceAsOne}
+        {this.props.handValue.aceAsOne !== this.props.handValue.aceAsEleven &&
+          " / " + this.props.handValue.aceAsEleven}
+      </span>;
 
-        // Set hand value text
-        const handValueDisplay =
-            this.props.handValue &&
-            <span className="handValue ms-font-xl">
-                Hand Value: {this.props.handValue.aceAsOne}
-                {this.props.handValue.aceAsOne !== this.props.handValue.aceAsEleven &&
-                    " / " + this.props.handValue.aceAsEleven}
-            </span>;
+    return (
+      <div className="DeckContainer">
+        <h3 className="ms-font-xl">
+          {this.props.title}{" "}
+          {this.props.handValue &&
+            <i
+              className="ms-Icon ms-Icon--Info"
+              onClick={this._toggleStatusCallout}
+              ref={calloutTarget => this._statusCalloutTarget = calloutTarget}
+            />}
+            {' '}
+          {toggleIcon}
+        </h3>
+        {this.state.isStatusCalloutVisible &&
+          <Callout
+            gapSpace={1}
+            targetElement={this._statusCalloutTarget}
+            onDismiss={this._toggleStatusCallout}
+            setInitialFocus={false}
+          >
+            <StatusDisplay
+              player={this.props.player}
+              gameStatus={this.props.gameStatus}
+              turnCount={this.props.turnCount}
+            />
+          </Callout>}
 
-        return (
-            <div className="DeckContainer">
-                <h3 className="ms-font-xl">
-                    {this.props.title}{' '}
-                    {this.props.handValue &&
-                        <i className="ms-Icon ms-Icon--Info"
-                            onClick={this._toggleCallout}
-                            ref={(calloutTarget) => this._menuButtonElement = calloutTarget}>&nbsp;</i>}
-                    {toggleIcon}
-                </h3>
-                {this.state.isCalloutVisible &&
-                    <Callout
-                        gapSpace={1}
-                        targetElement={this._menuButtonElement}
-                        onDismiss={this._toggleCallout}
-                        setInitialFocus={true}>
-                        <StatusDisplay
-                            player={this.props.player}
-                            gameStatus={this.props.gameStatus}
-                            turnCount={this.props.turnCount}
-                        />
-                    </Callout>
-                }
-                {this.state.deckIsVisible &&
-                    <Masonry
-                        className={"deck"}
-                        elementType={"div"}
-                        disableImagesLoaded={false}
-                        updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
-                        options={masonryOptions}
-                    >
-                        {childElements}
-                    </Masonry>}
-                {/*handValueDisplay*/}
-            </div>
-        );
-    }
+        {this.state.deckIsVisible &&
+          <Masonry
+            className={"deck"}
+            elementType={"div"}
+            disableImagesLoaded={false}
+            updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+            options={masonryOptions}
+          >
+            {childElements}
+          </Masonry>}
+      </div>
+    );
+  }
 }
 
 DeckContainer.propTypes = {
-    select: T.func,
-    deselect: T.func,
-    deck: T.array,
-    title: T.string,
-    handValue: T.object,
-    isSelectable: T.bool,
-    hidden: T.bool
+  select: T.func,
+  deselect: T.func,
+  deck: T.array,
+  title: T.string,
+  handValue: T.object,
+  isSelectable: T.bool,
+  hidden: T.bool,
+  player: T.object,
+  gameStatus: T.number,
+  turnCount: T.number
 };
 
 export default DeckContainer;
--- src/StatusDisplay.js
+++ src/StatusDisplay.js
@@ -4,8 +4,8 @@ import * as T from "prop-types";
 export const StatusDisplay = props => {
     return (
         <div id="StatusPanel" className="ms-font-s">
-            <span>Player: {props.player.title}</span><br />
-            <span>Status: {props.player.status}</span><br />
+            <span>Player: {props.player.title || ''}</span><br />
+            <span>Status: {props.player.status||''}</span><br />
             <span>Hand Value: {`${props.player.handValue.aceAsEleven} / ${props.player.handValue.aceAsOne}`}</span><br />
             <span>Turn: {props.player.turn || 'false'}</span><br />
             <span>Game Status: {props.gameStatus || 0}</span><br />
@@ -15,9 +15,9 @@ export const StatusDisplay = props => {
 };
 
 StatusDisplay.propTypes = {
-    gameStatus: T.number,
-    turnCount: T.number,
-    player: T.object
+    gameStatus: T.number.isRequired,
+    turnCount: T.number.isRequired,
+    player: T.object.isRequired
 };
 
 export default StatusDisplay;
