6f7e0d848cc9cc7cecff3fc32f1e98629cb5d89c
Chris Odegard <c.odegard@gmail.com>
2017-07-23 19:35:57 -0400

    troubleshooting import failure 2

--- src/components.js
+++ src/components.js
@@ -1,8 +1,1 @@
-// import { BaseComponent } from "./BaseComponent";
-// import { CardContainer } from "./CardContainer";
-// import { ControlPanel } from "./ControlPanel";
-// import { DeckContainer } from "./DeckContainer";
-// import { OptionsPanel } from "./OptionsPanel";
-// import { Player } from "./Player";
-// import { StatusDisplay } from "./StatusDisplay";
-export { Table } from "./Table";
+export * from "./Table";
