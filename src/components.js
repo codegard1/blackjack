64577a803110c05d6b10b7202064b0a75f9441cc
Chris Odegard <c.odegard@gmail.com>
2017-07-23 19:30:01 -0400

    troubleshooting import failure

--- src/components.js
+++ src/components.js
@@ -1,23 +1,8 @@
-import { BaseComponent } from "./BaseComponent";
-import { CardContainer } from "./CardContainer";
-import { ControlPanel } from "./ControlPanel";
-import { DeckContainer } from "./DeckContainer";
-import { OptionsPanel } from "./OptionsPanel";
-import { Player } from "./Player";
-import { StatusDisplay } from "./StatusDisplay";
-import { Table } from "./Table";
-
-module.exports = {
-  BaseComponent,
-  CardContainer,
-  ControlPanel,
-  DeckContainer,
-  OptionsPanel,
-  Player,
-  StatusDisplay,
-  Table
-};
-
-module.exports.printMsg = function() {
-  console.log("This is a message from the blackjack package.");
-};
+// import { BaseComponent } from "./BaseComponent";
+// import { CardContainer } from "./CardContainer";
+// import { ControlPanel } from "./ControlPanel";
+// import { DeckContainer } from "./DeckContainer";
+// import { OptionsPanel } from "./OptionsPanel";
+// import { Player } from "./Player";
+// import { StatusDisplay } from "./StatusDisplay";
+export { Table } from "./Table";
