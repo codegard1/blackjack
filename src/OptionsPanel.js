2bfdabf539ef6df526f1d0230df522a78694c555
Chris Odegard <christopher.odegard@macys.com>
2017-08-22 17:49:21 -0400

    added styles to OptionsPanel

--- src/OptionsPanel.js
+++ src/OptionsPanel.js
@@ -4,15 +4,18 @@ import { Panel, PanelType } from "office-ui-fabric-react/lib/Panel";
 import { Toggle } from "office-ui-fabric-react/lib/Toggle";
 import { CommandButton } from "office-ui-fabric-react/lib/Button";
 
+/* custom stuff */
+import './OptionsPanel.css';
+
 /* Flux */
 import AppActions from "./actions/AppActions";
 
-const OptionsPanel = (props) => {
+const OptionsPanel = props => {
   const resetGame = () => {
     AppActions.reset();
     AppActions.newDeck();
     AppActions.showMessageBar("Game Reset");
-  }
+  };
 
   return (
     <Panel
@@ -23,52 +26,63 @@ const OptionsPanel = (props) => {
       headerText="Options"
       isLightDismiss
     >
-      <CommandButton
-        iconProps={{ iconName: "Refresh" }}
-        disabled={false}
-        checked={false}
-        onClick={resetGame}
-      >
-        Reset Game
-      </CommandButton>
+      <div id="ButtonColumn">
+        <CommandButton
+          iconProps={{ iconName: "Refresh" }}
+          disabled={false}
+          checked={false}
+          onClick={resetGame}
+        >
+          Reset Game
+        </CommandButton>
 
-      <Toggle
-        checked={props.isDeckVisible}
-        label="Show Deck"
-        onAriaLabel="The deck is visible. Pres to hide it."
-        offAriaLabel="The deck is not visible. Press to show it."
-        onText="On"
-        offText="Off"
-        onChanged={checked => AppActions.toggleDeckVisibility(checked)}
-        value
-      />
-      <Toggle
-        checked={props.isDrawnVisible}
-        label="Show Drawn"
-        onAriaLabel="The Drawn cards are visible. Pres to hide it."
-        offAriaLabel="The Drawn cards are not visible. Press to show it."
-        onText="On"
-        offText="Off"
-        onChanged={checked => AppActions.toggleDrawnVisibility(checked)}
-      />
-      <Toggle
-        checked={props.isSelectedVisible}
-        label="Show Selected"
-        onAriaLabel="The Selected cards are visible. Pres to hide it."
-        offAriaLabel="The Selected cards are not visible. Press to show it."
-        onText="On"
-        offText="Off"
-        onChanged={checked => AppActions.toggleSelectedVisibility(checked)}
-      />
+        <CommandButton
+          iconProps={{ iconName: "StackIndicator" }}
+          disabled={false}
+          checked={false}
+          onClick={AppActions.deal}
+        >
+          Deal
+        </CommandButton>
+
+        <Toggle
+          checked={props.isDeckVisible}
+          label="Show Deck"
+          onAriaLabel="The deck is visible. Pres to hide it."
+          offAriaLabel="The deck is not visible. Press to show it."
+          onText="On"
+          offText="Off"
+          onChanged={checked => AppActions.toggleDeckVisibility(checked)}
+          value
+        />
+        <Toggle
+          checked={props.isDrawnVisible}
+          label="Show Drawn"
+          onAriaLabel="The Drawn cards are visible. Pres to hide it."
+          offAriaLabel="The Drawn cards are not visible. Press to show it."
+          onText="On"
+          offText="Off"
+          onChanged={checked => AppActions.toggleDrawnVisibility(checked)}
+        />
+        <Toggle
+          checked={props.isSelectedVisible}
+          label="Show Selected"
+          onAriaLabel="The Selected cards are visible. Pres to hide it."
+          offAriaLabel="The Selected cards are not visible. Press to show it."
+          onText="On"
+          offText="Off"
+          onChanged={checked => AppActions.toggleSelectedVisibility(checked)}
+        />
+      </div>
     </Panel>
   );
-}
+};
 
 OptionsPanel.propTypes = {
   isOptionsPanelVisible: T.bool.isRequired,
   isDeckVisible: T.bool.isRequired,
   isDrawnVisible: T.bool.isRequired,
-  isSelectedVisible: T.bool.isRequired,
+  isSelectedVisible: T.bool.isRequired
 };
 
 export default OptionsPanel;
