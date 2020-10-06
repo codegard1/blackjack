3d5e0c4580d7be209940492ec60c382332024c72
Chris Odegard <c.odegard@gmail.com>
2020-10-05 22:32:59 -0400

    restore default serviceWorker

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Table from "./components/blackjack/Table";
 
+// Register the serviceworker
+import * as serviceWorker from "./serviceWorker";
+serviceWorker.register();
+
 ReactDOM.render(<Table />, document.getElementById('root'));
