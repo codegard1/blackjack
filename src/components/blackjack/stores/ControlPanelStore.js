04975c0c6a9131d3ff81e444e6d77eaf74459bec
Chris Odegard <c.odegard@gmail.com>
2020-10-14 12:57:48 -0400

    add logic to get and update IDB

--- src/components/blackjack/stores/ControlPanelStore.js
+++ src/components/blackjack/stores/ControlPanelStore.js
@@ -2,7 +2,11 @@ import { EventEmitter } from "events";
 import AppDispatcher from "../dispatcher/AppDispatcher";
 import AppConstants from "../constants/AppConstants";
 
+import { defaultPlayers, defaultSelectedPlayerKey } from "../definitions";
+
 import { MessageBarType } from "office-ui-fabric-react/lib/MessageBar";
+
+import { get, set } from 'idb-keyval';
 
 /* state variables */
 let state = {
@@ -14,6 +18,8 @@ let state = {
   isMessageBarVisible: false,
   isOptionsPanelVisible: false,
   isSelectedVisible: false,
+  players: defaultPlayers,
+  selectedPlayerKey: defaultSelectedPlayerKey,
   messageBarDefinition: {
     type: MessageBarType.info,
     text: "",
@@ -26,25 +32,61 @@ let state = {
 /* Data, Getter method, Event Notifier */
 const CHANGE_EVENT = "controlPanel";
 const ControlPanelStore = Object.assign({}, EventEmitter.prototype, {
-  getState: function() {
+  getState: function () {
     return state;
   },
   /* This is redundant with AppActions.showMessageBar */
   /* TO DO: move this to GameStore, since only GameStore uses it. */
-  setMessageBar: function(text, type) {
+  setMessageBar: function (text, type) {
     _showMessageBar(text, type);
     this.emitChange(CHANGE_EVENT);
   },
-  emitChange: function() {
+  emitChange: function () {
     this.emit(CHANGE_EVENT);
+    console.log('ControlPanelStore.emitChange()');
   },
-  addChangeListener: function(callback) {
+  addChangeListener: function (callback) {
     this.on(CHANGE_EVENT, callback);
   },
-  removeChangeListener: function(callback) {
+  removeChangeListener: function (callback) {
     this.removeListener(CHANGE_EVENT, callback);
+  },
+  // Check for saved player data
+  async getSavedData() {
+
+    let players = await get('players');
+    let selectedPlayerKey = await get('selectedPlayerKey');
+
+    console.log(`players: ${JSON.stringify(players)}`);
+    console.log(`selectedPlayerKey: ${selectedPlayerKey}`);
+
+    // If no saved data is located
+    if (players === undefined) {
+      // Set saved data from default 
+      await set('players', defaultPlayers);
+    } else {
+      state.players = players
+    }
+
+    // If no saved data is located
+    if (selectedPlayerKey === undefined) {
+      // Set saved data from default 
+      await set('selectedPlayerKey', defaultSelectedPlayerKey);
+    } else {
+      state.selectedPlayerKey = selectedPlayerKey;
+    }
+  },
+  async updateSavedData(...keys) {
+    keys.forEach(key => {
+      set(key, state[key])
+        .then(() => console.log(`updated '${key}'`))
+        .catch(reason => console.log(`failed to update '${key}: ${reason}`));
+    });
   }
 });
+
+// Load saved data from IDB if it exists
+ControlPanelStore.getSavedData();
 
 /*  ========================================================  */
 /* register methods */
@@ -101,6 +143,12 @@ AppDispatcher.register(action => {
 
     case AppConstants.CONTROLPANEL_TOGGLECARDTITLEVISIBILITY:
       state.isCardDescVisible = action.bool;
+      ControlPanelStore.emitChange();
+      break;
+
+    case AppConstants.CONTROLPANEL_SELECTPLAYER:
+      state.selectedPlayerKey = action.key;
+      ControlPanelStore.updateSavedData("selectedPlayerKey");
       ControlPanelStore.emitChange();
       break;
 
