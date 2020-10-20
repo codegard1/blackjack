92ef19ca6e82f14655a9211c295dab21c57441a7
= <c.odegard@gmail.com>
2020-10-20 17:48:10 +0000

    add 'defaults' object

--- src/components/blackjack/definitions.js
+++ src/components/blackjack/definitions.js
@@ -3,9 +3,30 @@
  */
 
 export const defaultPlayers = [
-    { id: 1, title: "Chris", isNPC: false },
-    { id: 2, title: "Dealer", isNPC: true },
-    { id: 3, title: "John", isNPC: true },
+  { id: 1, title: "Chris", isNPC: false },
+  { id: 2, title: "Dealer", isNPC: true },
+  { id: 3, title: "John", isNPC: true },
 ];
 
 export const defaultSelectedPlayerKey = "chris";
+
+export const defaults = {
+  selectedPlayerKey: defaultSelectedPlayerKey,
+  players: defaultPlayers,
+  // default state of ControlPanelStore
+  controlPanelStore: {
+    isCardDescVisible: false,
+    isDealerHandVisible: false,
+    isDeckVisible: false,
+    isDrawnVisible: false,
+    isHandValueVisible: false,
+    isMessageBarVisible: false,
+    isOptionsPanelVisible: false,
+    isSelectedVisible: false,
+    players: defaultPlayers,
+    selectedPlayerKey: defaultSelectedPlayerKey,
+    newPlayerFieldValue: "",
+    isNewPlayerFieldEmpty: true,
+    isNewPlayerSaveButtonDisabled: false,
+  }
+}
