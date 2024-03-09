3c7ff7bc67d58922f949b5cbc44c60dfda7f82a5
Chris Odegard <c.odegard@gmail.com>
2024-03-09 15:07:42 -0500

    Convert boilerplate to TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 // React
-import * as React from 'react';
+import React from 'react';
 
 // FluentUI
-import { Stack } from '@fluentui/react';
+import { Stack, Text, Link, FontWeights, IStackTokens, IStackStyles, ITextStyles } from '@fluentui/react';
 
 // Local Resources
 import './App.css';
 import { CardContainer } from './components';
 import { PlayingCardDeck } from './classes';
+
+
+const boldStyle: Partial<ITextStyles> = { root: { fontWeight: FontWeights.semibold } };
+const stackTokens: IStackTokens = { childrenGap: 15 };
+const stackStyles: Partial<IStackStyles> = {
+  root: {
+    width: '960px',
+    margin: '0 auto',
+    textAlign: 'center',
+    color: '#605e5c',
+  },
+};
 
 // Main Component
 const App = () => {
@@ -15,10 +27,16 @@ const App = () => {
   // State
   const [deck, setDeck] = React.useState<PlayingCardDeck>(new PlayingCardDeck());
 
-  const cardsTest = deck.cards.map((c, i, a) => <CardContainer {...c} key={`card_${i}`} />);
+  const cardsTest = deck.cards.map((c, i, a) =>
+    <CardContainer {...c}
+      key={`card_${i}`}
+      isSelectable
+      isDescVisible
+    />
+  );
 
   return (
-    <Stack horizontal wrap horizontalAlign='space-between' verticalAlign='space-evenly' verticalFill>
+    <Stack tokens={stackTokens} styles={stackStyles} horizontal wrap horizontalAlign='space-between' verticalAlign='space-evenly' verticalFill>
       {cardsTest}
     </Stack>
   );
