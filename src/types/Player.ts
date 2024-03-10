604327ad6caa0f088bd6a01e9f789e1e3c50cb2b
Chris Odegard <c.odegard@gmail.com>
2024-03-10 17:00:40 -0400

    Player => BasePlayer

--- src/types/Player.ts
+++ src/types/Player.ts
@@ -1,14 +1,13 @@
-import { PlayingCard } from "../classes";
+import { PlayerHand } from ".";
 
-export type Player = {
-  bank: 1000;
-  disabled?: boolean;
+export type BasePlayer = {
+  bank: number;
   id: number;
+  disabled?: boolean;
+  hand: PlayerHand;
   isNPC: boolean;
   key: string;
-  selected: boolean;
   title: string;
-  hand: PlayingCard[];
 }
 
-export type PlayerKey = Player['key'];
+export type PlayerKey = BasePlayer['key'];
