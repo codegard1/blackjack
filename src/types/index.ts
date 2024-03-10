b51052cf3da4aa9d7d67f52946836ae6182738eb
Chris Odegard <c.odegard@gmail.com>
2024-03-10 17:00:51 -0400

    update types index

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,8 @@
 export type { Suit } from './Suit';
 export type { SuitKey } from './SuitKey';
 export type { SuitCollection, SuitCollectionKey } from './SuitCollection';
-export type { Player, PlayerKey } from './Player';
+export type { BasePlayer, PlayerKey } from './Player';
+export type { PlayerHand } from './PlayerHand';
+export type { PlayerHandValue } from './PlayerHandValue';
+export type { PlayerAction } from './PlayerAction';
+export type { PlayerCollection, PlayerCollectionKey } from './PlayerCollection';
