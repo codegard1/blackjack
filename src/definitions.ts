07b456e01d15b8fe8c4e5e9dd96afc698cec060f
Chris Odegard <c.odegard@gmail.com>
2024-03-11 14:12:54 -0400

    resolve linting errors

--- src/definitions.ts
+++ src/definitions.ts
@@ -3,16 +3,17 @@
  */
 
 import { IDropdownOption } from "@fluentui/react";
-import { Player, PlayerKey } from "./types";
+import { PlayerKey } from "./types";
+import { Player } from "./classes";
 
 
 
 //  Default players as array
 export const defaultPlayers: Player[] = [
-  { id: 1, key: "chris", title: "Chris", isNPC: false, disabled: false, selected: true, bank: 1000 },
-  { id: 2, key: "dealer", title: "Dealer", isNPC: true, disabled: false, selected: true, bank: 1000 },
-  { id: 3, key: "john", title: "John", isNPC: false, disabled: true, selected: false, bank: 1000 },
-  { id: 4, key: "ralph", title: "Ralph", isNPC: false, disabled: true, selected: false, bank: 1000 },
+  new Player('chris', 'Chris', false, 0),
+  new Player('chris', 'Delaer', false, 1),
+  new Player('john', 'John', false, 2),
+  new Player('ralph', 'Ralph', false, 3),
 ];
 
 
@@ -31,12 +32,16 @@ export const defaultSelectedPlayerKeys: PlayerKey[] = ['chris', 'dealer'];
 /**
  * default options for the splash dialog dropdown
  */
-export const defaultPlayersDropdownOptions: IDropdownOption[] = defaultPlayers.map(v => { return { text: v.title, ...v } })
-// [
-//   { key: 'chris', text: 'Chris', isNPC: false, isSelected: true },
-//   { key: 'dealer', text: 'Dealer', isNPC: true, isSelected: true },
-//   { key: 'john', text: 'John', isNPC: false, disabled: true, isSelected: false },
-//   { key: 'ralph', text: 'Ralph', isNPC: false, disabled: true, isSelected: false, },
-// ];
+export const defaultPlayersDropdownOptions: IDropdownOption[] = defaultPlayers.map(v => {
+  return {
+    text: v.title,
+    key: v.key,
+    id: v.id.toString(),
+    index: v.id,
+    title: v.title,
+    selected: (v.id <= 1),
+  }
+})
+
 
 export const defaultSelectedPlayerKey: PlayerKey = "chris";
