a9c7696999a8491c0e09e26f6f566d343b880740
Chris Odegard <c.odegard@gmail.com>
2024-03-11 17:52:35 -0400

    Set defaults for required properties in AppContext

--- src/classes/AppContext.ts
+++ src/classes/AppContext.ts
@@ -2,6 +2,17 @@ import React from 'react';
 import { PlayingCardDeck } from './';
 import { IAppContextProps } from '../interfaces';
 
-const AppContext = React.createContext<IAppContextProps>({ deck: new PlayingCardDeck() });
+const AppContext = React.createContext<IAppContextProps>({
+  deck: new PlayingCardDeck(),
+  isCardDescVisible: false,
+  isDealerHandVisible: false,
+  isHandValueVisible: false,
+  isDeckVisible: false,
+  isDrawnVisible: false,
+  isOptionsPanelVisible: false,
+  isSelectedVisible: false,
+  isCardTitleVisible: false,
+  isActivityLogVisible: false,
+});
 
 export default AppContext;
