96af3a0229dd571899864b09afde365cd5c985a0
Chris Odegard <c.odegard@gmail.com>
2024-03-12 12:38:31 -0400

    move enums into enums folder

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,5 @@
 export type { BasePlayer, PlayerKey } from './Player';
 export type { CardTuple } from './CardTuple';
-export type { PlayerAction } from './PlayerAction';
 export type { PlayerCollection, PlayerCollectionKey } from './PlayerCollection';
 export type { PlayerHand } from './PlayerHand';
 export type { PlayerHandValue } from './PlayerHandValue';
@@ -8,3 +7,4 @@ export type { PlayingCardKey } from './PlayingCardKey';
 export type { Suit } from './Suit';
 export type { SuitCollection, SuitCollectionKey } from './SuitCollection';
 export type { SuitKey } from './SuitKey';
+export type { PlayerStats, PlayerStatsKey } from './PlayerStats';
