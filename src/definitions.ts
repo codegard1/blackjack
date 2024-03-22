407b61dada58fe2ce2913f2767dce2d2e969ed45
Chris Odegard <c.odegard@gmail.com>
2024-03-22 17:44:01 -0400

    refactor default definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,47 +1,32 @@
-/**
- * Definitions
- */
-
 import { IDropdownOption } from "@fluentui/react";
-import { PlayerKey } from "./types";
 import { Player } from "./classes";
-
-
+import { PlayerKey } from "./types";
 
 //  Default players as array
 export const defaultPlayers: Player[] = [
-  new Player('chris', 'Chris', false, 0, 1000, 20),
-  new Player('chris', 'Delaer', false, 1, 1000, 20),
-  new Player('john', 'John', false, 2, 1000, 20),
-  new Player('ralph', 'Ralph', false, 3, 1000, 20),
-];
+  { key: 'chris', title: 'Chris', disabled: false, bank: 1000, isNPC: true },
+  { key: 'chris', title: 'Delaer', disabled: false, bank: 1000, isNPC: true },
+  { key: 'john', title: 'John', disabled: false, bank: 1000, isNPC: true },
+  { key: 'ralph', title: 'Ralph', disabled: false, bank: 1000, isNPC: true }
+].map((p, i) => new Player({ id: i, ...p }));
 
-
-// Default players as object
-// default state of PLayerStore
-export const defaultPlayersObj = () => defaultPlayers.slice();
-// { '1': { id: 1, key: 'chris', title: 'Chris', isNPC: false },
-// '2': { id: 2, key: 'dealer', title: 'Dealer', isNPC: true },
-// '3': { id: 3, key: 'ralph', title: 'Ralph', isNPC: false } }
 
 /**
  * default keys for the Dropdown on  the splash dialog
  */
-export const defaultSelectedPlayerKeys: PlayerKey[] = ['chris', 'dealer'];
+export const defaultSelectedPlayerKeys: PlayerKey[] = defaultPlayers.slice(0, 1).map((p) => p.key);
 
 /**
  * default options for the splash dialog dropdown
  */
 export const defaultPlayersDropdownOptions: IDropdownOption[] = defaultPlayers.map(v => {
   return {
+    ...v,
     text: v.title,
-    key: v.key,
     id: v.id.toString(),
     index: v.id,
-    title: v.title,
-    selected: (v.id <= 1),
+    selected: v.key in defaultSelectedPlayerKeys,
   }
 })
-
 
 export const defaultSelectedPlayerKey: PlayerKey = "chris";
