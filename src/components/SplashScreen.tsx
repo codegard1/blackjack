8064ea9b265e4508ebea95f810df950550764acf
Chris Odegard <c.odegard@gmail.com>
2024-03-25 18:03:13 -0400

    Dropdown state works properly

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -3,23 +3,22 @@ import React from "react";
 
 // Fluent UI
 import {
-  Dropdown,
   Dialog,
   DialogFooter,
   DialogType,
-  PrimaryButton,
+  Dropdown,
   IDropdownOption,
+  PrimaryButton,
 } from '@fluentui/react';
 
 import AppContext from "../classes/AppContext";
 
+import { Player } from "../classes";
 import {
-  defaultPlayers,
   defaultPlayersDropdownOptions,
   defaultSelectedPlayerKeys
 } from "../definitions";
 import { PlayerKey } from "../types";
-import { Player } from "../classes";
 
 export const SplashScreen: React.FC = () => {
 
@@ -27,36 +26,34 @@ export const SplashScreen: React.FC = () => {
   const {
     gameStore,
     settingStore,
+    playerStore,
   } = React.useContext(AppContext);
 
   // State
-  const [selectedPlayers, setSelectedPlayers] = React.useState<any>(defaultSelectedPlayerKeys);
+  const [selectedPlayers, setSelectedPlayers] = React.useState<PlayerKey[]>(defaultSelectedPlayerKeys);
 
 
   const onChangeDropDown = (event: React.FormEvent<HTMLDivElement>, option: IDropdownOption<Player> | undefined, index?: number): void => {
     if (option) {
       const _selectedPlayers = selectedPlayers.slice();
-      if (option!.selected && _selectedPlayers.indexOf(option!.key) === -1) {
-        _selectedPlayers.push(option!.key);
+      if (option!.selected && _selectedPlayers.indexOf(option!.key as PlayerKey) === -1) {
+        _selectedPlayers.push(option!.key as PlayerKey);
       } else if (!option?.selected && option!.key in selectedPlayers) {
-        _selectedPlayers.splice(_selectedPlayers.indexOf(option!.key), 1);
+        _selectedPlayers.splice(_selectedPlayers.indexOf(option!.key as PlayerKey), 1);
       }
       setSelectedPlayers(_selectedPlayers);
     }
   }
 
   const onClickStartButton = () => {
-    console.log('SplashScreen.onClickStartButton');
-    //   const { players } = props;
+    // get the complete player object for AppActions that don't use playerKey yet
+    // let pList = selectedPlayers.map(key => players[key]);
 
-    //   // get the complete player object for AppActions that don't use playerKey yet
-    //   // let pList = selectedPlayers.map(key => players[key]);
+    // initiate a new game 
+    // gameStore.newGame(pList);
 
-    //   // initiate a new game 
-    //   // gameStore.newGame(pList);
-
-    //   // hide the player selection modal 
-    //   props.onHide();
+    // hide the player selection modal 
+    settingStore.setSplashScreenVisible(false);
   }
 
   return (
@@ -81,6 +78,7 @@ export const SplashScreen: React.FC = () => {
         multiSelect
         onChange={onChangeDropDown}
         options={defaultPlayersDropdownOptions}
+        defaultSelectedKeys={defaultSelectedPlayerKeys}
       />
       <DialogFooter>
         <PrimaryButton text="Start" onClick={onClickStartButton} />
