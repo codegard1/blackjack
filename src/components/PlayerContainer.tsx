056ce603dd4847493098fed5999238a12db44ca4
Chris Odegard <c.odegard@gmail.com>
2024-03-26 11:46:46 -0400

    update PlayerContainer where player may be null

--- src/components/PlayerContainer.tsx
+++ src/components/PlayerContainer.tsx
@@ -2,10 +2,10 @@
 import React from "react";
 
 // Fluent UI
-import { Stack, Text } from "@fluentui/react";
+import { Layer, Stack, Text, nullRender } from "@fluentui/react";
 
 /* custom stuff */
-import { Agent, CardStack, DeckCallout, ControlPanel, StatusDisplay } from '.';
+import { Agent, CardStack, ControlPanel, DeckCallout, StatusDisplay } from '.';
 import { IPlayerContainerProps } from "../interfaces";
 
 // Styles
@@ -13,6 +13,7 @@ import "./PlayerContainer.css";
 
 // Context
 import AppContext from "../classes/AppContext";
+import { Player } from "../classes";
 
 // Component
 export const PlayerContainer: React.FC<IPlayerContainerProps> = (props) => {
@@ -26,31 +27,29 @@ export const PlayerContainer: React.FC<IPlayerContainerProps> = (props) => {
 
   // State
   const [isStatusCalloutVisible, setStatusCalloutVisible] = React.useState<boolean>(false);
-  const [isDeckCalloutVisible, setDeckCalloutVisible] = React.useState<boolean>(props.isDeckCalloutVisible);
+  const [isDeckCalloutVisible, setDeckCalloutVisible] = React.useState<boolean>(false);
+  const player = playerStore?.player(props.playerKey);
 
   const _showDeckCallout = () => setDeckCalloutVisible(true);
   const _hideDeckCallout = () => setDeckCalloutVisible(false);
 
-  const { player, playerKey } = props;
-  const handValue = player.hand.handValue;
 
   /* style PlayerContainer conditionally */
   let playerContainerClass = "PlayerContainer ";
-  if (player.turn) {
+  if (player?.turn) {
     playerContainerClass += "selected ";
   }
   if (
-    !player.empty &&
-    player.isStaying &&
-    !player.turn
+    player?.isStaying &&
+    !player?.turn
   ) {
     playerContainerClass += "staying ";
   }
 
-  const playerStatusFlag = (player.isBusted ||
-    player.isFinished ||
-    player.isStaying ||
-    !player.turn);
+  const playerStatusFlag = (player?.isBusted ||
+    player?.isFinished ||
+    player?.isStaying ||
+    !player?.turn);
 
   /* selectedFlag is true if getSelected() returns an array */
   // const selectedFlag = !!DeckStore.getSelected(playerKey);
@@ -59,36 +58,34 @@ export const PlayerContainer: React.FC<IPlayerContainerProps> = (props) => {
   return (
     <Stack className={playerContainerClass}>
 
-      <Stack horizontal horizontalAlign="space-between" style={{ padding: '5px' }} className={`${player.title}-titleBar playerContainerClass`}>
+      <Stack horizontal horizontalAlign="space-between" style={{ padding: '5px' }} className={`${player?.title}-titleBar playerContainerClass`}>
         <Stack.Item align="start">
           <Text block nowrap variant="large">
-            {`${player.title} ($${player.bank || 0})  `}</Text>
+            {`${player?.title} ($${player?.bank || 0})  `}</Text>
         </Stack.Item>
         <Stack.Item>
-          <StatusDisplay player={player} stats={player.stats} />
+          <StatusDisplay player={player} stats={player?.stats} />
         </Stack.Item>
       </Stack>
 
       <Stack horizontalAlign="space-between">
-        {player.isNPC && props.dealerHasControl &&
+        {player?.isNPC && gameStore.dealerHasControl &&
           <Stack.Item>
             <Agent
-              dealerHasControl={props.dealerHasControl}
+              dealerHasControl={gameStore.dealerHasControl}
               gameStatus={gameStore.gameStatus}
-              handValue={handValue}
-              playerKey={playerKey}
+              handValue={player?.handValue}
+              playerKey={props.playerKey}
             />
           </Stack.Item>
         }
 
-        {!player.isNPC &&
+        {!player?.isNPC &&
           <Stack.Item>
             <ControlPanel
-              gameStatus={gameStore.gameStatus}
-              hidden={player.isNPC}
-              minimumBet={props.minimumBet}
+              hidden={player? player.isNPC : false}
               player={player}
-              playerKey={playerKey}
+              playerKey={props.playerKey}
               playerStatusFlag={playerStatusFlag}
               selectedFlag={selectedFlag}
               showDeckCallout={_showDeckCallout}
@@ -97,22 +94,26 @@ export const PlayerContainer: React.FC<IPlayerContainerProps> = (props) => {
           </Stack.Item>
         }
 
-        <Stack.Item className={`DeckCalloutTarget-${player.title}`}>
-          <CardStack
-            cards={player.hand}
-            hidden={false}
-            isSelectable
-            player={player}
-            title={player.title}
-          />
+        <Stack.Item className={`DeckCalloutTarget-${player?.title}`}>
+          {player?.hand.cards ?
+            <CardStack
+              cards={player?.hand.cards}
+              hidden={false}
+              isSelectable
+              player={player}
+              title={player?.title}
+            /> : nullRender()
+          }
         </Stack.Item>
       </Stack>
-      <DeckCallout
-        player={player}
-        isDeckCalloutVisible={isDeckCalloutVisible}
-        onHideCallout={_hideDeckCallout}
-        target={`.DeckCalloutTarget-${player.title}`}
-      />
+      <Layer>
+        <DeckCallout
+          player={player}
+          isDeckCalloutVisible={isDeckCalloutVisible}
+          onHideCallout={_hideDeckCallout}
+          target={`.DeckCalloutTarget-${player?.title}`}
+        />
+        </Layer>
     </Stack>
   );
 }
