25f17c2c9dc8dcfce567c152f29e972a5bc197f1
Chris Odegard <c.odegard@gmail.com>
2024-03-29 16:46:51 -0400

    expand DeckAction enum

--- src/enums/DeckAction.ts
+++ src/enums/DeckAction.ts
@@ -2,7 +2,28 @@
  * Actions performed by DeckContext
  */
 export enum DeckAction {
+  // TODO
+  clearSelected = 'clearSelected',
+  // TODO
+  drawFromBottomOfDeck = 'drawFromBottomOfDeck',
+  DRAWONE = 'DRAWONE',
+  // TODO
+  drawRandom = 'drawRandom',
+  // TODO
+  putOnBottomOfDeck = 'putOnBottomOfDeck',
+  PUTONE = 'PUTONE',
+  // TODO
+  putOnTopOfDeck = 'putOnTopOfDeck',
+  // TODO
+  RESET = 'RESET',
   SELECT = 'SELECT',
-  DRAW = 'DRAW',
-  PUT = 'PUT',
+  // TODO
+  SHUFFLE = 'SHUFFLE',
+  // TODO
+  UNSELECT = 'UNSELECT',
+  // TODO
+  HIT = 'HIT',
+  // TO DO
+  newPlayerHand = 'newPlayerHand',
 }
+
