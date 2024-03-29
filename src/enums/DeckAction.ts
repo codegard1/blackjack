8a126e7a206141b3636918dd02430006df6cc37d
Chris Odegard <c.odegard@gmail.com>
2024-03-29 18:05:32 -0400

    remove redundant DeckAction enum values

--- src/enums/DeckAction.ts
+++ src/enums/DeckAction.ts
@@ -2,28 +2,13 @@
  * Actions performed by DeckContext
  */
 export enum DeckAction {
-  // TODO
   ClearSelected = 'ClearSelected',
-  // TODO
-  DrawFromBottomOfDeck = 'DrawFromBottomOfDeck',
-  DrawOne = 'DrawOne',
-  // TODO
-  DrawRandom = 'DrawRandom',
-  // TODO
-  PutOnBottomOfDeck = 'PutOnBottomOfDeck',
-  PutOne = 'PutOne',
-  // TODO
-  PutOnTopOfDeck = 'PutOnTopOfDeck',
-  // TODO
+  Draw = 'Draw',
+  NewPlayerHand = 'NewPlayerHand',
+  Put = 'Put',
   Reset = 'Reset',
   Select = 'Select',
-  // TODO
   Shuffle = 'Shuffle',
-  // TODO
   Unselect = 'Unselect',
-  // TODO
-  Hit = 'Hit',
-  // TO DO
-  NewPlayerHand = 'NewPlayerHand',
 }
 
