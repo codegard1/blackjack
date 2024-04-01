7be6f1c08825166cbb9026bbb71d440697cc21f6
Chris Odegard <c.odegard@gmail.com>
2024-04-01 19:51:17 -0400

    remove gameStore and playerStore from AppContext

--- src/classes/AppContext.ts
+++ src/classes/AppContext.ts
@@ -1,42 +1,13 @@
 // React
 import React from 'react';
 
-// Fluent UI
-import { MessageBarType } from '@fluentui/react';
-
 // Local Resources
 import { IAppContextProps } from '../interfaces';
-import { PlayerKey } from '../types';
 
 // Create the default Context object with mostly dummy values
 const AppContext = React.createContext<IAppContextProps>({
   initializeStores: () => { },
   clearStores: () => { },
-  playerStore: null,
-  gameStore: {
-    bet: (key: PlayerKey) => console.log('bet'),
-    deal: (key: PlayerKey) => console.log('deal'),
-    hideMessageBar: () => console.log('hideMessageBar'),
-    hit: (key: PlayerKey) => console.log('hit'),
-    lastWriteTime: '',
-    loser: undefined,
-    newActivityLogItem: (name, description, iconName) => { },
-    newGame: (keys: PlayerKey[]) => { },
-    newRound: () => { },
-    pot: 0,
-    resetGame: () => console.log('resetGame'),
-    round: 0,
-    setLastWriteTime: () => console.log('setLastWriteTime'),
-    setLoser: () => console.log('setLoser'),
-    setPot: () => console.log('setPot'),
-    setRound: () => console.log('setRound'),
-    setTurnCount: () => console.log('setTurnCount'),
-    setWinner: () => console.log('setWinner'),
-    showMessageBar: () => console.log('showMessageBar'),
-    stay: () => console.log('stay'),
-    turnCount: 0,
-    winner: undefined,
-  }
 });
 
 export default AppContext;
--- src/interfaces/IAppContextProps.ts
+++ src/interfaces/IAppContextProps.ts
@@ -1,9 +1,5 @@
-import { IGameStoreProps } from '.';
-import { PlayerStore } from '../classes';
 
 export interface IAppContextProps {
   initializeStores: () => void;
   clearStores: () => void;
-  playerStore: PlayerStore | null;
-  gameStore: IGameStoreProps;
 }
