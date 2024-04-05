847ceb69f455469b942a13f1c027e4d8aa6798c5
Chris Odegard <c.odegard@gmail.com>
2024-04-05 17:11:37 -0400

    update gameReducer to call PlayingCardDeck class methods

--- src/functions/gameReducer.ts
+++ src/functions/gameReducer.ts
@@ -1,3 +1,4 @@
+import { PlayingCard } from "../classes";
 import { gameDefaults } from "../context/gameDefaults";
 import { GameStatus } from "../enums";
 import { GameAction } from "../enums/GameAction";
@@ -13,10 +14,13 @@ import { GameState, PlayerStats } from "../types";
 export function gameReducer(state: GameState, action: IGameReducerAction) {
 
   const {
+    cardKey,
     controllingPlayerKey,
+    deckSide,
     gameStatus,
     messageBarDefinition,
     minimumBet,
+    numberOfCards,
     playerKey,
     potIncrement,
     type,
@@ -251,7 +255,108 @@ export function gameReducer(state: GameState, action: IGameReducerAction) {
       return state;
     }
 
+    // Clear the list of selected cards
+    case GameAction.ClearSelected: {
+      state.deck.clearSelected();
+      return state;
+    }
 
+    // Create an entry in the playerHands list for the given Player
+    case GameAction.NewPlayerHand: {
+      // Single Player
+      if (undefined !== playerKey && typeof playerKey === 'string')
+        state.deck.newPlayerHand(playerKey);
+      // Multiple Players
+      if (undefined !== playerKey && typeof playerKey !== 'string') {
+        playerKey.forEach((pk) => state.deck.newPlayerHand(pk));
+      }
+      return state;
+    }
+
+    // Select a card
+    case GameAction.Select: {
+      // If no cardKey was passed then do nothing 
+      if (undefined !== cardKey) state.deck.select(cardKey);
+      return state;
+    }
+
+    // Unselect a card
+    case GameAction.Unselect: {
+      if (undefined !== cardKey) state.deck.unselect(cardKey);
+      return state;
+    }
+
+    // Draw card(s) from the deck
+    case GameAction.Draw: {
+      const _num = (undefined === numberOfCards) ? 0 : numberOfCards;
+      if (state.deck.length > _num) {
+
+        switch (deckSide) {
+          case 'top': {
+            const _drawn = state.deck.draw(_num);
+            if (undefined !== playerKey && typeof playerKey === 'string')
+              state.deck.playerHands[playerKey].cards.push(..._drawn);
+            break;
+          }
+          case 'random': {
+            const _drawn = state.deck.drawRandom(_num);
+            if (undefined !== playerKey && typeof playerKey === 'string')
+              state.deck.playerHands[playerKey].cards.push(..._drawn);
+            break;
+          }
+          default: {
+            const _drawn = state.deck.drawFromBottomOfDeck(_num);
+            if (undefined !== playerKey && typeof playerKey === 'string')
+              state.deck.playerHands[playerKey].cards.push(..._drawn);
+            break;
+          }
+        }
+      } else {
+        throw new Error('There are not enough cards left in the deck to draw');
+      }
+
+      return state;
+    }
+
+    // Put one card into the deck
+    case GameAction.Put: {
+
+      // If no cardKey was passed then do nothing
+      if (undefined === cardKey) return state;
+
+      // If cardKey is not in the deck already then put it
+      if (!(state.deck.has(cardKey))) {
+
+        // deckSide determines where in the cardKeys array the cardKey goes
+        if (undefined !== deckSide) {
+          switch (deckSide) {
+            case 'top':
+              state.deck.putOnTopOfDeck([new PlayingCard(cardKey)])
+              break;
+            case 'random':
+              state.deck.putInMiddleOfDeck([new PlayingCard(cardKey)]);
+              break;
+            default:
+              state.deck.putOnBottomOfDeck([new PlayingCard(cardKey)]);
+              break;
+          }
+        }
+      }
+
+      return state;
+    }
+
+    // Shuffle the remaining card in the deck
+    case GameAction.Shuffle: {
+      state.deck.shuffle();
+      return state;
+    }
+
+    // Populate and shuffle the deck
+    case GameAction.Reset: {
+      state.deck.reset();
+      return state;
+    }
 
     default: {
       return state;
