801266e2fcedf3bc12b84fbdaf5b47f5fed54066
Chris Odegard <c.odegard@gmail.com>
2024-04-05 11:50:59 -0400

    SettingContext consumes settingDefaults

--- src/context/SettingContext.ts
+++ src/context/SettingContext.ts
@@ -1,6 +1,7 @@
 import { createContext } from 'react';
 import { SettingsState } from '../types';
 import { ISettingReducerAction } from '../interfaces';
+import { settingDefaults } from './settingDefaults';
 
-export const SettingContext = createContext<SettingsState>({});
+export const SettingContext = createContext<SettingsState>(settingDefaults);
 export const SettingDispatchContext = createContext<React.Dispatch<ISettingReducerAction>>(() => { });
