84d60dc2b6a0e4698f163eba323eb540ef892e41
Chris Odegard <c.odegard@gmail.com>
2024-04-08 19:01:20 -0400

    merge gameDefaults into GameContext

--- src/context/GameContext.ts
+++ src/context/GameContext.ts
@@ -1,7 +1,36 @@
+import { MessageBarType } from '@fluentui/react';
 import { Dispatch, createContext, useContext } from 'react';
+import { PlayerStore, PlayingCardDeck } from '../classes';
+import { defaultplayersArr } from '../definitions';
+import { GameStatus } from '../enums';
 import { IGameReducerAction } from '../interfaces';
 import { GameState } from '../types';
-import { gameDefaults } from './gameDefaults';
+
+export const gameDefaults: GameState = {
+  gameStatus: GameStatus.Init,
+  deck: new PlayingCardDeck(),
+  gameStatusFlag: false,
+  isSpinnerVisible: false,
+  loser: undefined,
+  winner: undefined,
+  dealerHasControl: false,
+  minimumBet: 25,
+  pot: 0,
+  round: 0,
+  turnCount: 0,
+  lastWriteTime: new Date().toISOString(),
+  messageBarDefinition: {
+    type: MessageBarType.info,
+    text: "",
+    isMultiLine: false
+  },
+  controllingPlayer: undefined,
+  players: defaultplayersArr,
+  activePlayerKeys: undefined,
+  currentPlayerKey: undefined,
+  playerStore: new PlayerStore(),
+}
+
 
 export const GameContext = createContext<GameState>(gameDefaults);
 export const GameDispatchContext = createContext<Dispatch<IGameReducerAction>>(() => { });
