cca524b3357c2ed1e0c17e3262366462b4ac3eab
Chris Odegard <c.odegard@gmail.com>
2024-04-09 14:19:41 -0400

    refactor ControlPanel

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -5,8 +5,8 @@ import React from "react";
 import { CommandBar, ICommandBarItemProps, MessageBarType, nullRender } from "@fluentui/react";
 
 // Local Resources
-import { useGameContext } from "../context";
-import { GameAction, GameStatus } from "../enums";
+import { useGameContext, useSettingContext } from "../context";
+import { GameAction } from "../enums";
 import { IControlPanelProps } from "../interfaces";
 
 // Component
@@ -16,143 +16,84 @@ export const ControlPanel: React.FC<IControlPanelProps> = (props) => {
   const {
     playerStatusFlag,
     playerKey,
+    selectedFlag,
   } = props;
 
   // State from context
   const { gameState, gameDispatch } = useGameContext();
-  const { gameStatusFlag, gameStatus, minimumBet,currentPlayerKey } = gameState;
+  const { settings, toggleSetting } = useSettingContext();
+  const {
+    currentPlayerKey,
+    deck,
+    gameStatus,
+    gameStatusFlag,
+    minimumBet,
+    playerStore,
+  } = gameState;
 
   // Computed properties
-  const player = gameState.playerStore.player(props.playerKey);
+  const player = playerStore.player(props.playerKey);
   const npcFlag = player.isNPC;
+  const isBtnDisabled = gameStatus > 2 || currentPlayerKey !== playerKey;
 
   /* selectedFlag is true when the player has selected cards in his hand */
-  // let selectedFlag = this.props.selectedFlag;
   /* Flag used by put / draw menu items */
-  // let selectedCards = selectedFlag
-  // ? DeckStore.getSelected(playerKey)
-  //   : [];
+  // let selectedCards = selectedFlag ? DeckStore.getSelected(playerKey) : [];
   /* when gameStatusFlag is TRUE, most members of blackJackItems are disabled */
 
-  // const drawItems = [
-  //   {
-  //     key: "draw",
-  //     name: "Draw",
-  //     ariaLabel: "Draw",
-  //     iconProps: "",
-  //     disabled: false,
-  //     onClick: AppActions.draw
-  //   },
-  //   {
-  //     key: "draw-from-bottom-of-deck",
-  //     name: "Draw from Bottom of Deck",
-  //     ariaLabel: "Draw from Bottom of Deck",
-  //     iconProps: "",
-  //     disabled: false,
-  //     onClick: AppActions.drawFromBottomOfDeck
-  //   },
-  //   {
-  //     key: "draw-random",
-  //     name: "Draw Random",
-  //     ariaLabel: "Draw Random",
-  //     iconProps: "",
-  //     disabled: false,
-  //     onClick: AppActions.drawRandom
-  //   }
-  // ];
-  // const putItems = [
-  //   {
-  //     key: "put-on-top-of-deck",
-  //     name: "Put on Top of Deck",
-  //     ariaLabel: "Put on Top of Deck",
-  //     iconProps: "",
-  //     disabled: false,
-  //     onClick: ev => {
-  //       ev.preventDefault();
-  //       AppActions.putOnTopOfDeck(playerKey, selectedCards);
-  // AppActions.removeSelectedFromPlayerHand(playerKey,selectedCards);
-  //     }
-  //   },
-  //   {
-  //     key: "put-on-bottom-of-deck",
-  //     name: "Put on Bottom of Deck",
-  //     ariaLabel: "Put on Bottom of Deck",
-  //     iconProps: "",
-  //     disabled: false,
-  //     onClick: cards => {
-  //       AppActions.putOnBottomOfDeck(cards);
-  //       AppActions.removeSelectedFromPlayerHand(playerKey,cards);
-  //     }
-  //   }
-  // ];
+  const drawItems = [
+    {
+      key: "draw",
+      name: "Draw",
+      ariaLabel: "Draw",
+      iconProps: { iconName: "HandsFree" },
+      disabled: isBtnDisabled,
+      onClick: () => gameDispatch({ type: GameAction.Draw, playerKey, deckSide: 'top', numberOfCards: 1 }),
+    },
+    {
+      key: "draw-from-bottom-of-deck",
+      name: "Draw from Bottom of Deck",
+      ariaLabel: "Draw from Bottom of Deck",
+      iconProps: { iconName: "HandsFree" },
+      disabled: isBtnDisabled,
+      onClick: () => gameDispatch({ type: GameAction.Draw, playerKey, deckSide: 'bottom', numberOfCards: 1 }),
+    },
+    {
+      key: "draw-random",
+      name: "Draw Random",
+      ariaLabel: "Draw Random",
+      iconProps: { iconName: "HandsFree" },
+      disabled: isBtnDisabled,
+      onClick: () => gameDispatch({ type: GameAction.Draw, playerKey, deckSide: 'random', numberOfCards: 1 }),
+    },
+  ];
+
   let blackJackItems: ICommandBarItemProps[] = [
     {
       key: "hit",
       name: "Hit",
       ariaLabel: "Hit",
       iconProps: { iconName: "Add" },
-      disabled: gameStatus > 2 || currentPlayerKey !== playerKey,
-      onClick: (ev: any) => {
-        ev.preventDefault();
-        gameDispatch({ type: GameAction.Draw, playerKey, numberOfCards: 1, deckSide: 'top' });
-      }
+      disabled: isBtnDisabled,
+      onClick: () => gameDispatch({ type: GameAction.Draw, playerKey, numberOfCards: 1, deckSide: 'top' }),
     },
-    // {
-    //   key: "bet",
-    //   name: `Bet $${this.props.minimumBet}`,
-    //   ariaLabel: `Bet $${this.props.minimumBet}`,
-    //   iconProps: { iconName: "Up" },
-    //   disabled: (gameStatusFlag || playerStatusFlag),
-    //   onClick: (ev, target, playerIndex, amount) => {
-    //     ev.preventDefault();
-    //     AppActions.bet(playerKey, amount);
-    //   }
-    // },
+    {
+      key: "bet",
+      name: `Bet $${minimumBet}`,
+      ariaLabel: `Bet $${minimumBet}`,
+      iconProps: { iconName: "Up" },
+      disabled: isBtnDisabled,
+      onClick: () => gameDispatch({ type: GameAction.SetMinimumBet, playerKey, minimumBet }),
+    },
     {
       key: "stay",
       name: "Stay",
       ariaLabel: "Stay",
       iconProps: { iconName: "Forward" },
-      disabled: gameStatusFlag || playerStatusFlag,
-      onClick: () => gameDispatch({ type: GameAction.Stay }),
+      disabled: isBtnDisabled,
+      onClick: () => gameDispatch({ type: GameAction.Stay, playerKey }),
     }
   ];
-
-  /* menu items for drawing cards */
-  // const drawMenu = [
-  //   {
-  //     key: "deck-menu",
-  //     name: "Draw",
-  //     ariaLabel: "Draw",
-  //     iconProps: "",
-  //     onClick(ev) {
-  //       ev.preventDefault();
-  //     },
-  //     subMenuProps: {
-  //       items: drawItems,
-  //       isSubMenu: true,
-  //       isBeakVisible: true
-  //     }
-  //   }
-  // ];
-
-  /* menu items for putting cards */
-  // const putMenu = [
-  //   {
-  //     key: "put-menu",
-  //     name: "Put",
-  //     ariaLabel: "Put",
-  //     iconProps: "",
-  //     onClick(ev) {
-  //       ev.preventDefault();
-  //     },
-  //     subMenuProps: {
-  //       items: putItems,
-  //       isSubMenu: true,
-  //       isBeakVisible: true
-  //     }
-  //   }
-  // ];
 
   const farItems: ICommandBarItemProps[] = npcFlag
     ? []
@@ -162,10 +103,10 @@ export const ControlPanel: React.FC<IControlPanelProps> = (props) => {
         name: "Deal",
         ariaLabel: "Deal",
         iconProps: { iconName: "Refresh" },
-        disabled: gameStatusFlag,
+        disabled: gameStatus > 0,
         onClick: () => {
-          gameDispatch({ type: GameAction.ResetGame });
           gameDispatch({ type: GameAction.NewRound });
+          toggleSetting({ key: 'isMessageBarVisible', value: true });
           gameDispatch({
             type: GameAction.ShowMessageBar,
             messageBarDefinition: { text: "New Round", type: MessageBarType.info, isMultiLine: false }
@@ -174,8 +115,7 @@ export const ControlPanel: React.FC<IControlPanelProps> = (props) => {
       }
     ];
 
-  // const overFlowItems = selectedFlag ? [].concat(putMenu, drawMenu) : [];
-  const overFlowItems: ICommandBarItemProps[] = [];
+  const overFlowItems: ICommandBarItemProps[] = [...drawItems];
 
   return props.hidden ? nullRender() :
     <CommandBar
@@ -185,4 +125,3 @@ export const ControlPanel: React.FC<IControlPanelProps> = (props) => {
       className="player-controlpanel"
     />
 }
-
